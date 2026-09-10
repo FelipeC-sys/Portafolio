7a5e4dca26dae3b1e49d858eac0af7631ef69d45
Andrés F. Carvajal <9felipegc@gmail.com>
2026-09-10 17:33:37 -0500

    Configurar PWA para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Portafolio/',
+
   plugins: [
     react(),
 
@@ -16,15 +18,17 @@ export default defineConfig({
         theme_color: '#0f172a',
         background_color: '#0f172a',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/Portafolio/',
+        scope: '/Portafolio/',
+
         icons: [
           {
-            src: '/icon-192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512.png',
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
